e2fb5750b4e0a90b9f1155df77515ea0b86142a3
Shameem12344 <153969524+Shameem12344@users.noreply.github.com>
2024-06-13 10:31:40 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let state = reactive({message: 'Hello Universe'})
+
+function renderApp(){ 
+render('#container', `<h1>${state.message}</h1>`)
+}
+
+renderApp()
+
+setTimeout(() => {
+  state.message = 'Hello World'
+}, 1000);
+
